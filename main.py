a824cbd9d2b5212d03c4134162697e967548ec90
Ulises Castellanos <200300580@ucaribe.edu.mx>
2023-09-05 18:14:36 -0500

    se agrego el canal y ruido

--- main.py
+++ main.py
@@ -1,43 +1,69 @@
+import pygame
+import io
+import os
+import random
+
 def emisor():
   with open('prue.mp3', 'rb') as mp3_file:
     contenido_binario = mp3_file.read()
   return contenido_binario
 
-def inversor(contenido_binario):
+def transmisor(contenido_binario):
   return bytes([~x & 0xFF for x in contenido_binario])
+
+def receptor(contenido_binario):
+  return bytes([~x & 0xFF for x in contenido_binario])
+
+
+def introducir_ruido(paquete, probabilidad_ruido):
+  paquete_con_ruido = bytearray(paquete)
+  for i in range(len(paquete_con_ruido)):
+    if random.random() < probabilidad_ruido:
+      paquete_con_ruido[i] = random.randint(0, 255)
+  return bytes(paquete_con_ruido)
 
 def dividir_paquetes(contenido_binario, t_paquete):
   paquetes = []
-  n=0
+  n = 0
   for i in range(0, len(contenido_binario), t_paquete):
-    n+=1
-    #print("d", n)
+    n += 1
+    print("d", n)
     paquete = contenido_binario[i:i + t_paquete]
     paquetes.append(paquete)
   return paquetes
 
 def unir_paquetes(paquetes):
-  n=0
+  n = 0
   contenido_binario = b""
   for paquete in paquetes:
-    n+=1
-    #print("u", n)
+    n += 1
+    print("u", n)
     contenido_binario += paquete
   return contenido_binario
 
-def Destino(contenido_binario):
-  with open('resultado.mp3', 'wb') as mp3_file:
-    mp3_file.write(contenido_binario)
+def canal(contenido_i, t_paquete, probabilidad_ruido):
+  paquetes = dividir_paquetes(contenido_i, t_paquete)
+  paquetes_con_ruido = [introducir_ruido(paquete, probabilidad_ruido) for paquete in paquetes]
+  contenido_reensamblado = unir_paquetes(paquetes_con_ruido)
+  return contenido_reensamblado
 
-t_paquete = 4
+def Destino(contenido_binario):
+  os.environ['SDL_AUDIODRIVER'] = 'directsound'
+  pygame.init()
+  mp3_stream = io.BytesIO(contenido_binario)
+  pygame.mixer.music.load(mp3_stream)
+  pygame.mixer.music.play()
+  while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
+t_paquete = 1024
+probabilidad_ruido = 0.0001  
 
 contenido_binario = emisor()
-contenido_i = inversor(contenido_binario)
+contenido_i = transmisor(contenido_binario)
 
-paquetes = dividir_paquetes(contenido_i, t_paquete)
+contenido_reensamblado = canal(contenido_i, t_paquete, probabilidad_ruido)
 
-contenido_reensamblado = unir_paquetes(paquetes)
-
-contenido_original = inversor(contenido_reensamblado)
+contenido_original = receptor(contenido_reensamblado)
 
 Destino(contenido_original)
